e0e46428b7eda4a119ef52b86ad96365ad505e9b
osama khaireldin <osama@cosmoland.ai>
2023-09-14 15:31:23 +0300

    new commit

--- main.js
+++ main.js
@@ -7,14 +7,7 @@
 -- boolean
 */
 
-console.log('osama khair');
-console.log(typeof 'osama khair');
-console.log(typeof 5000);
-console.log(typeof 5000.99);
-console.log(typeof [13, 32,11]);
-console.log(typeof ['cosmo','osos', 'loka']);
-console.log(typeof{name: 'osama', age: 20, country: 'EG'});
-console.log(typeof true);
-console.log(typeof false);
-console.log(typeof undefined);
-console.log(typeof null);
+
+const names = ['cosmo', 'loka']
+
+console.log(names)
