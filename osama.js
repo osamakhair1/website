c30479cf7fba54fcaf0b611089bd6658bee42f98
osama khaireldin <osama@cosmoland.ai>
2023-09-16 18:34:12 +0300

    array

---
+++ osama.js
@@ -0,0 +1,13 @@
+let thename = "samarahmedmahmoudmohamed";
+
+console.log(thename);
+console.log(thename[3]);
+console.log(thename[23]);
+console.log(thename.charAt(21));
+console.log(thename.length);
+console.log(thename.trim());
+
+console.log(thename.toUpperCase())
+console.log(thename.toLocaleLowerCase())
+console.log(thename.trim().charAt(13).toUpperCase())
+console.log(thename.trim().charAt(22).toLocaleLowerCase())
---
+++ work.js
@@ -0,0 +1,9 @@
+const colors = ["red", "green", "blue"];
+
+console.log(colors.at(-1));
+
+console.log(colors.at(-2));
+
+console.log(colors.slice(2));
+
+console.log(colors.slice(-2));
